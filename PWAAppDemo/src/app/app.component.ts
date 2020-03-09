1a8feecf3645335f244aef337ecc2d51b2009ecc
mahak23 <you@example.com>
2020-03-09 21:25:35 +0530

    PWA App demo

---
+++ PWAAppDemo/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+import { SwUpdate } from "@angular/service-worker";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "PWAAppDemo";
+  update: boolean = false;
+  constructor(updates: SwUpdate) {
+    updates.available.subscribe(event => {
+      this.update = true;
+    });
+  }
+}
